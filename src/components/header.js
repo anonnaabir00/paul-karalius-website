1b98215cbec8f3983db8d1625cc73a26b87f08d3
anonnaabir00 <anonnaabir00@gmail.com>
2020-06-23 21:52:51 +0600

    Header Component Redesigned

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,67 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+//import { Link } from "gatsby"
+//import PropTypes from "prop-types"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import React, { Component } from "react";
+import { graphql, StaticQuery } from 'gatsby';
+import '../css/bootstrap.min.css';
+import '../css/style.css';
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+class Header extends Component {
+  
+  // responsiveTrack = () => {
+  //   var x = document.getElementById("header-navigation");
+  //     if (x.className == "topnav") {
+  //         x.className += " responsive";
+  //         console.log('Class Changed');
+  // } else {
+  //   //x.className = "vertical-menu";
+  //   console.log('Class Not Changed');
+  // }
+  // };
 
-export default Header
+    showDropdown = () => {
+      document.getElementById("show-dropdown").style.display = "block";
+      console.log('Alhamdulillah. It works.')
+    }
+
+    render() {
+      return (
+        <StaticQuery 
+        query={graphql`
+        query SiteMenu {
+          allWordpressWpMenu {
+            nodes {
+              title
+              url
+            }
+          }
+        }
+        `}
+        render={data => (
+          <div>
+          <div class="desktop-menu" id="header-navigation">
+            {data.allWordpressWpMenu.nodes.map(menus => {
+              return(
+                <a href={menus.url}>{menus.title}</a>
+              )})}
+          </div>
+          <div class="mobile-menu">
+            <a href="#" class="icon" onClick={this.showDropdown}>Menu</a>
+            <ul class="mobile-dropdown" id="show-dropdown">
+            {data.allWordpressWpMenu.nodes.map(menus => {
+              return(
+                <li><a href={menus.url}>{menus.title}</a></li>
+              )})}
+            </ul>
+            </div>
+  </div>
+        )}
+        />
+
+        );
+    }
+  }
+
+ 
+  export default Header;
