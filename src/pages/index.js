b6b488bd2ebf967fa6b50dc7776f04d8b6693ff8
anonnaabir00 <anonnaabir00@gmail.com>
2020-06-30 01:52:09 +0600

    Updated
    - Added State
    - Added Conditional Rendering (Callery And Carousel)

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,26 +7,44 @@ import { Link } from "gatsby"
 
 import Header from "../components/header";
 import Gallery from "../components/gallery";
+import CarouselSlider from "../components/carousel";
 
 import '../css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-class IndexPage extends Component {  
+  
+
+class IndexPage extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        output: <CarouselSlider />,
+      }
+    }
+  
+  changeView = () => {
+    console.log("Alhamdulillah, view changed")
+    this.setState({output: <Gallery />})
+  }
+
   render() {
     return (
-        <Container fluid>
-          <Row>
-          <Col>
+      <div class="container-fluid">
+          <div class="row flex">
+          <div class="col-2">
           <Header />
-            </Col>
-            <Col>
-            <Gallery />
-            </Col>
-            </Row>
-        </Container>
-      
+            <a href="#" onClick={this.changeView}>
+            <h4 class="fixed-bottom">SHOW THUMBNAILS</h4>
+            </a>
+          </div>
+          <div class="col-10">
+            {this.state.output}
+          </div>
+          
+          </div>
+          </div>
       );
   }
 }
