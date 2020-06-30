becbc93e44d5461e8a21f00a7873051c7a29c3f7
anonnaabir00 <anonnaabir00@gmail.com>
2020-07-01 01:49:02 +0600

    Updated
    - Added Slick Carousal

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 import { graphql, StaticQuery } from 'gatsby';
 
-import '../css/bootstrap.min.css';
+
 import Carousel from 'react-bootstrap/Carousel'
 
+import RBCarousel from "react-bootstrap-carousel";
+import '../css/bootstrap.css';
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+
+import Slider from "react-slick";
+import '../css/slick.css';
+
 class CarouselSlider extends Component {
+  constructor(props) {
+    super(props);
+    this.nextSlide = this.nextSlide.bind(this);
+    this.previousSlide = this.previousSlide.bind(this);
+  }
+
+
+  nextSlide = () => {
+    console.log("Call Done");
+    this.slider.slickNext();
+  }
+
+  previousSlide = () => {
+    this.slider.slickPrev();
+  }
+
     render(){
         return(
             <StaticQuery
@@ -32,22 +55,28 @@ class CarouselSlider extends Component {
             `}
 
             render={data => (
-                        <Carousel
-                        nextLabel='Next'
-                        wrap={true}
-                        className="carousel-fade"
-                        >
+                          <div>
+                          <Slider
+                          ref={c => (this.slider = c)}
+                          dots={false}
+                          arrows={false}
+                          fade={true}
+                          infinite={true}
+                          speed={500}
+                          slidesToShow={1}
+                          slidesToScroll= {1}
+                          newSlide={this.nextSlide}
+                          >
                         {data.allWordpressWpPortfolio.edges.map(portfolio => {
                             return(
-                                <Carousel.Item>
                                 <img
                                 className="carousel-image"
                                 src={portfolio.node.acf.image.localFile.childImageSharp.fluid.srcWebp}
                                 alt={portfolio.node.acf.project_name}
                                     />
-                                 </Carousel.Item>
                             )})}
-            </Carousel>
+                            </Slider>
+              </div>
             )}
               
             />
