b2335c5d405cb38d7065871c913f2a8cc36165be
anonnaabir00 <anonnaabir00@gmail.com>
2020-07-07 12:54:25 +0600

    Added
    - Dark Mode Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,7 @@ module.exports = {
 
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-dark-mode",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
